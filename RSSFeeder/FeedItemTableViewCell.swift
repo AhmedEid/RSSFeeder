8db868f552a6b66007948fd43cd023d987dc3ca5
Ahmed Eid <supereid86@gmail.com>
2015-03-11 09:23:40 -0700

    Top align description label for feed items in menu

--- RSSFeeder/FeedItemTableViewCell.swift
+++ RSSFeeder/FeedItemTableViewCell.swift
@@ -15,6 +15,7 @@ class FeedItemTableViewCell: UITableViewCell {
     @IBOutlet weak var feedItemDescriptionLabel: UILabel!
     @IBOutlet weak var feedItemPublishedDateLabel: UILabel!
     
+    @IBOutlet weak var feedItemDescriptionLabelHeightConstraint: NSLayoutConstraint!
     override func awakeFromNib() {
         selectionStyle = .None
         self.feedItemPublishedDateLabel.textColor = UIColor(rgba: "#b1b1b1")
@@ -25,6 +26,20 @@ class FeedItemTableViewCell: UITableViewCell {
             self.feedItemTitleLabel.text = self.item?.feedItemName
             self.feedItemDescriptionLabel.text = self.item?.feedItemDescription;
             self.feedItemPublishedDateLabel.text = self.item?.feedItemPublishedString
+            
+            self.feedItemDescriptionLabelHeightConstraint.constant = min(heightForView(self.item!.feedItemDescription, font: self.feedItemDescriptionLabel.font, width: self.feedItemDescriptionLabel.bounds.size.width), CGFloat(40))
+            layoutIfNeeded()
         }
+    }
+    
+    func heightForView(text:String, font:UIFont, width:CGFloat) -> CGFloat{
+        let label:UILabel = UILabel(frame: CGRectMake(0, 0, width, CGFloat.max))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        label.font = font
+        label.text = text
+        
+        label.sizeToFit()
+        return label.frame.height
     }
 }
